f880b7299ff2bfaaa0dba8dbb6774726b8800065
rkkim04 <loiskim175@gmail.com>
2024-11-25 10:23:49 +0000

    goroutine concept 2

--- main.go
+++ main.go
@@ -9,24 +9,14 @@ import (
 // where saving the results
 func main(){
 	c := make(chan string)
-	people := [2]string{"nico","flynn"}
+	people := [5]string{"nico","flynn","dal","japanguy","jarry"}
 	// create goroutines
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	resultOne := <-c
-	resultTwo := <-c
-	resultThree := <-c
-	fmt.Println("Waiting for messages.")
-	// blocking operations. 끝날때까지 멈춤 여기서
-	// once c get a message then move to next line
-	fmt.Println("Received this message:", resultOne)
-	// and then moved to next and wait for the return
-	fmt.Println("Received this message:", resultTwo)
-	// and main func ends
-
-	// goroutines 는 갯수만큼만 동작하므로 더 많이 입력하면 에러가 발생함
-	fmt.Println("Received this message:", resultThree)
+	for i:=0;i<len(people);i++{
+		fmt.Println(<-c)
+	}
 }
 
 func isSexy(person string, c chan string) {
