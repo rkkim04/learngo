64b09cdeb3307408c2ea6c8f28d5fa8233b47aa6
loisrk <loiskim150@gmail.com>
2024-12-03 21:59:58 +0900

    backup

--- main.go
+++ main.go
@@ -2,54 +2,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/rkkim04/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("requset failed")
+
+
 func main(){
-	// account := accounts.NewAccount("lois")
-	// account.Deposit(1000)
-	// err := account.Withdraw(2000)
-	// if err != nil{
-	// 	log.Println(err)
-	// }
-	// fmt.Println(account)
-
-	dictionary := mydict.Dictionary{}
-	// definition, err := dictionary.Search("first")
-	// if err != nil{
-	// 	fmt.Println(err)
-	// }else{
-	// 	fmt.Println(definition)
-	// }
-	// word := "hello"
-	// definition := "Greeting"
-	// err := dictionary.Add(word, definition)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// Search()로 Dictionary에 word가 있는지 확인
-	// hello, _ := dictionary.Search(word)
-	// fmt.Println("found", word, "definition:", hello)
-
-	// 단어추가: 이미 있는 단어인 경우 미리 지정한 err 메세지 출력
-	// err2 := dictionary.Add(word, definition)
-	// if err2 != nil {
-	// 	fmt.Println(err2)
-	// }
-
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+	var results = map[string]string{
+		
 	}
-	fmt.Println(word)
+
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results{
+		fmt.Println(url, result)
+	}
+}
+
+
+
+// hitURL test
+func hitURL(url string) error {
+	fmt.Println("checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
 
